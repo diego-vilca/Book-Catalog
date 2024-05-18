6cdea1bf85ec998d424de7f01f6e38faa930cc70
Diego Vilca <vicv87.mp@gmail.com>
2024-05-18 17:23:31 -0300

    chore: initial Spring Boot project setup and database configuration
    - Created a new Spring Boot project with the basic structure
    - Added dependencies for Spring Data JPA and PostgreSQL Driver
    - Configured application properties for database connection

---
+++ literalura/src/test/java/com/diegovilca/literalura/LiteraluraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diegovilca.literalura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiteraluraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
