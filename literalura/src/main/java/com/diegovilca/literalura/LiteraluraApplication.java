e70014365dbd1b59c992a2c943ac3239c12afbed
Diego Vilca <vicv87.mp@gmail.com>
2024-05-21 17:33:40 -0300

    feat: add console menu

--- literalura/src/main/java/com/diegovilca/literalura/LiteraluraApplication.java
+++ literalura/src/main/java/com/diegovilca/literalura/LiteraluraApplication.java
@@ -1,18 +1,22 @@
 package com.diegovilca.literalura;
 
+import com.diegovilca.literalura.principal.Principal;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Scanner;
 
 @SpringBootApplication
 public class LiteraluraApplication implements CommandLineRunner {
 
-    public static void main(String[] args) {
-        SpringApplication.run(LiteraluraApplication.class, args);
-    }
+	public static void main(String[] args) {
+		SpringApplication.run(LiteraluraApplication.class, args);
+	}
 
-    @Override
-    public void run(String... args) throws Exception {
-    }
+	@Override
+	public void run(String... args) throws Exception {
+		Principal principal = new Principal();
+		principal.showMenu();
+	}
 }
--- literalura/src/main/java/com/diegovilca/literalura/principal/Principal.java
+++ literalura/src/main/java/com/diegovilca/literalura/principal/Principal.java
@@ -1,4 +1,59 @@
 package com.diegovilca.literalura.principal;
 
+import com.diegovilca.literalura.service.BookService;
+
+import java.util.Scanner;
+
 public class Principal {
+    private final String URL_BASE = "https://gutendex.com/books/";
+    private final Scanner scanner = new Scanner(System.in);
+    private final BookService bookService;
+
+    public Principal() {
+        bookService = new BookService();
+    }
+
+    public void showMenu() {
+        var option = -1;
+        while (option != 0) {
+            String menu = """
+                    
+                    +---------------------------------------+
+                    |                Main Menu              |
+                    +---------------------------------------+
+                    | 1. Find book by title                 |
+                    | 2. List registered books              |
+                    | 3. List registered authors            |
+                    | 4. List authors alive in a given year |
+                    | 5. List books by language             |
+                    | 0. Exit                               |
+                    +---------------------------------------+
+                    Enter your choice: """;
+            System.out.println(menu);
+            option = scanner.nextInt();
+            scanner.nextLine();
+            switch (option) {
+                case 1:
+                    bookService.findBookByTitle(URL_BASE);
+                    break;
+                case 2:
+
+                    break;
+                case 3:
+
+                    break;
+                case 4:
+
+                    break;
+                case 5:
+
+                    break;
+                case 0:
+                    System.out.println("Closing app...");
+                    break;
+                default:
+                    System.out.println("Invalid input. Please enter a number: ");
+            }
+        }
+    }
 }
